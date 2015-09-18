f7054b4209b471f400e162e72c2e317bd7387b8b
s-kontyava <s.kontyava@gmail.com>
2015-09-18 17:13:41 +0300

    Prepare to add Control.Excepptio

--- CSVParse/src/Main.hs
+++ CSVParse/src/Main.hs
@@ -1,3 +1,6 @@
+
+
+
 data User = User { firstName 
                  , lastName 
                  , email :: String
@@ -5,10 +8,15 @@ data User = User { firstName
 changeEmail :: User -> String -> User
 changeEmail user newEmail = user { email = newEmail }
 
-main =           
-       let userWithNewEmail = changeEmail user "s.kontyava@gmail.com"
-       in putStrLn $ email userWithNewEmail
-       where user = User {  firstName = "Sergey"
-                          , lastName = "Kontyava"
-                          , email    = "iisquideii@gmail.com"
-                         }
+main :: IO ()
+main =  do
+        userInput <- getLine
+        putStrLn $ "Not bad at all" ++ userInput
+--       let userWithNewEmail = changeEmail user "s.kontyava@gmail.com"
+--      in putStrLn $ email userWithNewEmail
+--       where user = User {  firstName = "Sergey"
+--                          , lastName = "Kontyava"
+--                         , email    = "iisquideii@gmail.com"
+--                         }
+
+
