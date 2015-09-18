ec7c73f54d8f319de7cb981d9711639c7ec30655
s-kontyava <s.kontyava@gmail.com>
2015-09-18 15:11:54 +0300

    Class-like stuff made

--- CSVParse/src/Main.hs
+++ CSVParse/src/Main.hs
@@ -1,1 +1,13 @@
-main = putStrLn "CSVStuff"
+data User = User { firstName :: String
+                 , lastName  :: String
+                 , email     :: String
+                 }
+
+main = putStrLn $ firstName user ++ " " ++
+                  lastName user ++ ", " ++
+                  email user
+
+       where user = User { firstName = "Sergey"
+                   ,lastName  = "Kontyava"
+                   , email    = "iisquideii@gmail.com"
+                  }
